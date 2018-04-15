56a98d7d6aae7cd042a59ab74c2c739ad4d1099b
Kui Chen <kuiche@microsoft.com>
2018-04-14 20:07:59 -0700

    add rx type and pack

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import * as fs from 'fs';
 import * as cluster from 'cluster';
+import * as Rx from 'rx';
 var currentDir = process.cwd();
 console.log(currentDir);
 console.log(cluster.isMaster);
 export class Book {
     constructor(public name:string, public price:number){}
 }
+Rx.Observable.just('hello world').subscribe(function(value:string){
+    console.log(value);
+})
